1bd668df90687f01eefe15d77525a16690621220
test01 - HoaiThanh <thanhthanhdecember@gmail.com>
2026-07-17 11:56:59 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,12 @@
+let powerUp = "flower";
+let effect = "";
+
+if (powerUp === "mushroom") {
+    effect = "Mario becomes Supper!";
+} else if (powerUp === "flower") {
+    effect = "Mario can shoot fireballs!";
+} else if (powerUp === "start") {
+    effect = "Mario Invincible!";
+}
+
+console.log(effect);
